05e10f6bc7b7aac14517cd84788a36ad68f316e6
Rishikesh Rishi <rishikeshrishi@Rishikeshs-MacBook-Pro.local>
2024-04-06 18:37:22 +0530

    Added one more endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/todo")
+def todo():
+    return jsonify({"name": "rishi", "age": 26})
 
 
 if __name__ == "__main__":
